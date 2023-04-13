96d6e7aa43ce15ef7cc49b6a60adf895a6d8ec43
LYH <mangham9987@gmail.com>
2023-04-13 21:56:50 +0900

    업로드연습4

--- src/Main.java
+++ src/Main.java
@@ -24,9 +24,6 @@ public class Main {
 			
 			List<Article> articles = new ArrayList<>();
 			
-			Article article = new Article(id, title, body);
-			
-			articles.add(article);
 			
 		 
 		 while(true) {
@@ -37,32 +34,35 @@ public class Main {
 				 if (articles.size() == 0) {
 					System.out.println("존재하는 게시글이 없습니다");
 					continue;
-				} else 
+				}
 					
 				System.out.println("번호    |  제목");
 				 
 				for (int i = articles.size() - 1; i >= 0; i--) {
-
+					Article article = articles.get(i);
 					System.out.printf("%d  	 |  %s\n", article.id, article.title);
 
 				}
+			} else if(cmd.equals("article write")) {
+				int id = lastArticleId + 1;
+				lastArticleId = id;
+				System.out.printf("제목 : ");
+				String title = sc.nextLine();
+				System.out.printf("내용 : ");
+				String body = sc.nextLine();
+
+				Article article = new Article(id, title, body);
+				
+				articles.add(article);
+				
+				System.out.printf("%d번글이 생성되었습니다\n", id);
+			}
+				
 			} else {
 				System.out.println("존재하지 않는 명령어 입니다");
 			}
 			
-			 if(cmd.equals("article write")) {
-					int id = lastArticleId + 1;
-					lastArticleId = id;
-					System.out.printf("제목 : ");
-					String title = sc.nextLine();
-					System.out.printf("내용 : ");
-					String body = sc.nextLine();
-
-
-				
-					
-					System.out.printf("%d번글이 생성되었습니다\n", id);
-			}
+			 
 			
 			 if(cmd.equals("exit")) {
 				 break;
