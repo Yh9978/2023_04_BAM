a62a0a15f3974800ba9f270051375d61badcce86
unknown <mangham9978@gmail.com>
2023-04-13 19:54:26 +0900

    git 업로드 연습

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		
+		 Scanner sc = new Scanner(System.in);
+		
+		 System.out.println("==Program Start==");
+		 
+		 String cmd = sc.nextLine();
+//		 sc.nextInt();
+//		 sc.next();
+		 
+		 System.out.println(cmd);
+		 
+		 System.out.println("==Program Quit==");
+
+		 
+		 
+	}
+}
