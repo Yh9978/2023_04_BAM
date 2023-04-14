661ebd84616ed5060d67ee939b48e62a5b389970
LYH <mangham9987@gmail.com>
2023-04-14 20:18:45 +0900

    강사님 코드 수정

--- src/Main.java
+++ src/Main.java
@@ -1,95 +1,88 @@
 //   2023/4/11 오후 19시 웹개발 수업
 //   Workspace 이름 eclipes-work_space
 
-import java.util.Scanner;
 import java.util.ArrayList;
 import java.util.List;
-
+import java.util.Scanner;
 public class Main {
-	@SuppressWarnings("hiding")
-	public static <Article> void main(String[] args) {
-		
-		 Scanner sc = new Scanner(System.in);
-		
-		 System.out.println("==Program Start==");
-		 
-		 String command;
-		 
-		 command = sc.next();
+	public static void main(String[] args) {
+		System.out.println("== 프로그램 시작 ==");
+		Scanner sc = new Scanner(System.in);
+		int lastArticleId = 0;
+		List<Article> articles = new ArrayList<>();
+		while (true) {
+			System.out.printf("명령어) ");
+			String cmd = sc.nextLine();
+			if (cmd.equals("exit")) {
+				break;
+			}
 
-		 int num = sc.nextInt();
-			System.out.printf("입력된 정수 : %d\n", num);
-			int lastArticleId = 0;
-			lastArticleId = num;
-			
-			List<Article> articles = new ArrayList<>();
-			
-			
-		 
-		 while(true) {
-			 System.out.println("명령어");
-			 String cmd = sc.nextLine();
-			 
-			 if(cmd.equals("article list")) {
-				 if (articles.size() == 0) {
-					System.out.println("존재하는 게시글이 없습니다");
-					continue;
-				}
-					
-				System.out.println("번호    |  제목");
-				 
-				for (int i = articles.size() - 1; i >= 0; i--) {
-					Article article = articles.get(i);
-					System.out.printf("%d  	 |  %s\n", article.id, article.title);
-
-				}
-			} else if(cmd.equals("article write")) {
+			if (cmd.equals("article write")) {
+				System.out.println("== 게시물 작성 ==");
 				int id = lastArticleId + 1;
 				lastArticleId = id;
-				System.out.printf("제목 : ");
-				String title = sc.nextLine();
-				System.out.printf("내용 : ");
-				String body = sc.nextLine();
+				System.out.printf("제목 : "); 
+				
+			{
 
-				Article article = new Article(id, title, body);
-				
-				articles.add(article);
-				
-				System.out.printf("%d번글이 생성되었습니다\n", id);
-			}
-				
+				}
+
+			if (cmd.equals("article list")) {
+			
+				if (articles.size() == 0) {
+					System.out.println("존재하는 게시물이 없습니다");
+					continue;
+				}
+				System.out.println("== 게시물 목록 ==");
+				System.out.println("번호	|	제목	");
+
+				for (int i = articles.size() - 1; i >= 0; i--) {
+					Article article = articles.get(i);
+
+					System.out.printf("%d	|	%s	\n", article.id, article.title);
+				}
+			
+			} else if (cmd.startsWith("article detail ")) {
+
+				String[] cmdBits = cmd.split(" ");
+				int id = Integer.parseInt(cmdBits[2]);
+
+				Article foundArticle = null;
+
+				for (Article article : articles) {
+					if (article.id == id) {
+						foundArticle = article;
+						break;
+					}
+				}
+
+				if (foundArticle == null) {
+					System.out.printf("%d번 게시물은 존재하지 않습니다\n", id);
+					continue;
+				}
+
+				System.out.println("== 게시물 상세보기 ==");
+				System.out.printf("번호 : %d\n", foundArticle.id);
+				System.out.printf("제목 : %s\n", foundArticle.title);
+				System.out.printf("내용 : %s\n", foundArticle.body);
 			} else {
 				System.out.println("존재하지 않는 명령어 입니다");
 			}
-			
-			 
-			
-			 if(cmd.equals("exit")) {
-				 break;
-			 }
-		 }
-		 
-		 sc.close();
-		 
-		 System.out.println("==Program Quit==");
-
-		 
-		 
-	
-
+		}
+		sc.close();
+		System.out.println("== 프로그램 끝 ==");
 	}
 }
 class Article {
-		int id;
-		String title;
-		String body;
-
-		public Article(int id, String title, String body) {
-			this.id = id;
-			this.title = title;
-			this.body = body;
-		}
+	int id;
+	String title;
+	String body;
+	public Article(int id, String title, String body) {
+		this.id = id;
+		this.title = title;
+		this.body = body;
 	}
+}
 
 // article id,title,body에 에러가 있다.
 // 근데 왜이러는지 진짜 모르겠다
